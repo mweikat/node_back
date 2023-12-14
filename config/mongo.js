50b1521a55a88edec09a2c8344a4a8cb7a8634ea
jose luis <mweikat@hotmail.com>
2023-12-14 18:36:24 -0300

    fixes

--- config/mongo.js
+++ config/mongo.js
@@ -1,18 +1,34 @@
-const mongoose = require("mongoose")
+const { MongoClient } = require('mongodb');
+
+// URL de conexión a MongoDB.
+const uri = process.env.DBMongoUri;
+
+// Opciones de conexión
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+// Crear un cliente de MongoDB.
+const client = new MongoClient(uri, options);
+
+
 
 const dbMongo = () => {
 
-    const dbMongoUri = process.env.DBMongoUri
-    mongoose.connect(dbMongoUri, {
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    },(err, res)=>{
-        if(!err)
-            console.log("*********CONECTION OK**************")
-        else
-            console.log("*********CONECTION FAIL**************")
-    })
+        // Conectar al servidor de MongoDB.
+        client.connect()
+        .then(() => {
+        console.log('Conexión exitosa a la base de datos');
 
+        // Puedes realizar operaciones en la base de datos aquí.
+
+        // Finalizar la conexión cuando hayas terminado de usarla.
+        client.close();
+        })
+        .catch(error => {
+        console.error('Error al conectar a la base de datos:', error);
+        });
 }
 
 module.exports = dbMongo
